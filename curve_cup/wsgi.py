d72722e591e823d43b6934b09068c66dfb0579c9
kianoosh.abbasi76 <kianoosh.abbasi76@gmail.com>
2016-12-11 22:37:50 +0330

    initial commit

---
+++ curve_cup/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for curve_cup project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "curve_cup.settings")
+
+application = get_wsgi_application()
